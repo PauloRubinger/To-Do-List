d7f7e12ea8cce8a3403ab2ecb1633f2a1c6d4926
Paulo Rubinger <paulopimentarubinger@gmail.com>
2024-06-25 04:08:51 -0300

    Add tradução da biblioteca antd

--- code/front-end/src/App.jsx
+++ code/front-end/src/App.jsx
@@ -1,9 +1,15 @@
 import HomePage from "./pages/HomePage";
+import { ConfigProvider } from "antd";
+import ptBR from 'antd/lib/locale/pt_BR';
 
 function App() {
   return (
-    <HomePage>
-    </HomePage>
+    <ConfigProvider
+      locale={ptBR}
+    >
+      <HomePage>
+      </HomePage>
+    </ConfigProvider>
   );
 }
 
