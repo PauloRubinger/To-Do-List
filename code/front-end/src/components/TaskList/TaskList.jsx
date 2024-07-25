d0ccab3f4902bf37d948e366053a0d2b33e823b1
Paulo Victor Pimenta Rubinger <paulopimentarubinger@gmail.com>
2024-07-25 00:52:23 -0300

    Feature: Add priority filter

--- code/front-end/src/components/TaskList/TaskList.jsx
+++ code/front-end/src/components/TaskList/TaskList.jsx
@@ -1,28 +1,44 @@
-import { useState, useEffect } from 'react';
-import { Card, List, Typography, Row, Col, Select, ConfigProvider, Empty } from 'antd';
-import { Task } from '../Task/Task';
-import styles from './TaskList.module.css';
-import { listAllByTaskList } from '../../services/api';
-import AddButton from '../AddButton/AddButton';
-import { ModalAddTask } from '../../components/ModalAddTask/ModalAddTask';
-import editIcon from '../../assets/images/editing.svg';
-import deleteIcon from '../../assets/images/delete.svg';
-import { ModalEditTaskList } from '../ModalEditTaskList/ModalEditTaskList';
-import { ModalDeleteTaskList } from '../ModalDeleteTaskList/ModalDeleteTaskList';
-import { FilterFilled } from '@ant-design/icons'
+import { useState, useEffect } from "react";
+import {
+  Card,
+  List,
+  Typography,
+  Row,
+  Col,
+  Select,
+  ConfigProvider,
+  Empty,
+} from "antd";
+import { Task } from "../Task/Task";
+import styles from "./TaskList.module.css";
+import { listAllByTaskList } from "../../services/api";
+import AddButton from "../AddButton/AddButton";
+import { ModalAddTask } from "../../components/ModalAddTask/ModalAddTask";
+import editIcon from "../../assets/images/editing.svg";
+import deleteIcon from "../../assets/images/delete.svg";
+import { ModalEditTaskList } from "../ModalEditTaskList/ModalEditTaskList";
+import { ModalDeleteTaskList } from "../ModalDeleteTaskList/ModalDeleteTaskList";
+import { FilterFilled } from "@ant-design/icons";
 
 const { Title, Text } = Typography;
 
-export const TaskList = ({ taskListId, title, description, onTaskListUpdated, onTaskListDeleted }) => {
+export const TaskList = ({
+  taskListId,
+  title,
+  description,
+  onTaskListUpdated,
+  onTaskListDeleted,
+}) => {
   const [tasks, setTasks] = useState([]);
   const [isModalAddTaskOpen, setIsModalAddTaskOpen] = useState(false);
   const [isModalEditTaskListOpen, setIsModalEditTaskListOpen] = useState(false);
-  const [isModalDeleteTaskListOpen, setIsModalDeleteTaskListOpen] = useState(false);
+  const [isModalDeleteTaskListOpen, setIsModalDeleteTaskListOpen] =
+    useState(false);
 
   const taskList = {
     id: taskListId,
     name: title,
-    description: description
+    description: description,
   };
 
   useEffect(() => {
@@ -45,11 +61,17 @@ export const TaskList = ({ taskListId, title, description, onTaskListUpdated, on
   };
 
   const handleTaskUpdated = (updatedTask) => {
-    setTasks((prevTasks) => prevTasks.map((prevTask) => prevTask.id === updatedTask.id ? updatedTask : prevTask));
+    setTasks((prevTasks) =>
+      prevTasks.map((prevTask) =>
+        prevTask.id === updatedTask.id ? updatedTask : prevTask
+      )
+    );
   };
 
   const handleTaskDeleted = (deletedTask) => {
-    setTasks((prevTasks) => prevTasks.filter((prevTask) => prevTask.id !== deletedTask.id));
+    setTasks((prevTasks) =>
+      prevTasks.filter((prevTask) => prevTask.id !== deletedTask.id)
+    );
   };
 
   const handleCloseAddTaskModal = () => {
@@ -83,67 +105,141 @@ export const TaskList = ({ taskListId, title, description, onTaskListUpdated, on
   return (
     <>
       <div className={styles.taskListContainer}>
-        <Card 
+        <Card
           styles={{
             body: {
-              paddingTop: 0
+              paddingTop: 0,
             },
             header: {
               paddingTop: 16,
-              paddingBottom: 16
-            }
+              paddingBottom: 16,
+            },
           }}
           className={styles.cardContainer}
           title={
             <div className={styles.cardHeader}>
               <Row align="middle" className={styles.cardText}>
                 <Col>
-                  <Title level={2} className={styles.cardTitle}>{title}</Title>
+                  <Title level={2} className={styles.cardTitle}>
+                    {title}
+                  </Title>
                   <Text className={styles.cardDescription}>{description}</Text>
-                  {tasks.length !== 0 && (
-                    <div className={styles.dateFilter}>
-                      <Select 
-                        placeholder={
-                          <div className={styles.dateFilterPlaceholder}>
-                            <FilterFilled />
-                            <span>Data da conclusão</span>
-                          </div>
-                        }
-                        options={[
-                          {
-                            value: "closest",
-                            label: "Mais próxima"
-                          },
-                          { 
-                            value: "further",
-                            label: "Mais distante"
-                          }
-                        ]}
-                      /> 
-                    </div>
+                  {tasks.length > 0 && (
+                    <Row align={"bottom"} gutter={20}>
+                      <Col>
+                        <div className={styles.dateFilter}>
+                          <Select
+                            placeholder={
+                              <div className={styles.filterPlaceholder}>
+                                <FilterFilled />
+                                <span>Filtrar por data da conclusão</span>
+                              </div>
+                            }
+                            options={[
+                              {
+                                value: "closest",
+                                label: "Prazos mais próximos",
+                              },
+                              {
+                                value: "further",
+                                label: "Prazos mais distantes",
+                              },
+                            ]}
+                          />
+                        </div>
+                      </Col>
+
+                      <Col>
+                        <div className={styles.priorityFilter}>
+                          <Select
+                            placeholder={
+                              <div className={styles.filterPlaceholder}>
+                                <FilterFilled />
+                                <span>Filtrar por prioridade</span>
+                              </div>
+                            }
+                            options={[
+                              {
+                                value: "highestPriority",
+                                label: "Maior prioridade",
+                              },
+                              {
+                                value: "lowestPriority",
+                                label: "Menor prioridade",
+                              },
+                            ]}
+                          ></Select>
+                        </div>
+                      </Col>
+                    </Row>
                   )}
                 </Col>
+
                 <Col className={styles.cardActions}>
-                  <img src={editIcon} alt="Editar lista de tarefas" onClick={handleEditTaskList} className={styles.editIcon} />
-                  {isModalEditTaskListOpen && <ModalEditTaskList taskList={taskList} modalOpen={true} onClose={handleCloseEditTaskListModal} onTaskListUpdated={handleTaskListUpdated} />}
-                  <img src={deleteIcon} alt="Excluir lista de tarefas" onClick={handleDeleteTaskList} className={styles.deleteIcon} />
-                  {isModalDeleteTaskListOpen && <ModalDeleteTaskList taskList={taskList} modalOpen={true} onClose={handleCloseDeleteTaskListModal} onTaskListDeleted={handleTaskListDeleted} />}
+                  <img
+                    src={editIcon}
+                    alt="Editar lista de tarefas"
+                    onClick={handleEditTaskList}
+                    className={styles.editIcon}
+                  />
+                  {isModalEditTaskListOpen && (
+                    <ModalEditTaskList
+                      taskList={taskList}
+                      modalOpen={true}
+                      onClose={handleCloseEditTaskListModal}
+                      onTaskListUpdated={handleTaskListUpdated}
+                    />
+                  )}
+                  <img
+                    src={deleteIcon}
+                    alt="Excluir lista de tarefas"
+                    onClick={handleDeleteTaskList}
+                    className={styles.deleteIcon}
+                  />
+                  {isModalDeleteTaskListOpen && (
+                    <ModalDeleteTaskList
+                      taskList={taskList}
+                      modalOpen={true}
+                      onClose={handleCloseDeleteTaskListModal}
+                      onTaskListDeleted={handleTaskListDeleted}
+                    />
+                  )}
                 </Col>
               </Row>
             </div>
-          }>
-          <ConfigProvider renderEmpty={() => <Empty description="Esta lista ainda não possui tarefas" />}>
+          }
+        >
+          <ConfigProvider
+            renderEmpty={() => (
+              <Empty description="Esta lista ainda não possui tarefas" />
+            )}
+          >
             <List
               dataSource={tasks}
-              renderItem={task => (
+              renderItem={(task) => (
                 <List.Item key={task.id}>
-                  <Task {...task} onTaskUpdated={handleTaskUpdated} onTaskDeleted={handleTaskDeleted} />
+                  <Task
+                    {...task}
+                    onTaskUpdated={handleTaskUpdated}
+                    onTaskDeleted={handleTaskDeleted}
+                  />
                 </List.Item>
               )}
             />
           </ConfigProvider>
-          <AddButton label={"Nova Tarefa"} className={styles.addTaskButton} onClick={handleAddTask} />
-          {isModalAddTaskOpen && <ModalAddTask taskListId={taskListId} modalOpen={true} onClose={handleCloseAddTaskModal} onTaskAdded={handleTaskAdded} />}
+          <AddButton
+            label={"Nova Tarefa"}
+            className={styles.addTaskButton}
+            onClick={handleAddTask}
+          />
+          {isModalAddTaskOpen && (
+            <ModalAddTask
+              taskListId={taskListId}
+              modalOpen={true}
+              onClose={handleCloseAddTaskModal}
+              onTaskAdded={handleTaskAdded}
+            />
+          )}
         </Card>
       </div>
     </>
