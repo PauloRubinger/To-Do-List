66a4e60fc6dd30c10dcf22931c38c8626d0346a3
Paulo Victor Pimenta Rubinger <paulopimentarubinger@gmail.com>
2024-08-06 23:38:13 -0300

    test: fix task list controller tests

--- code/back-end/src/test/java/com/todolist/unit/controller/TaskListControllerTest.java
+++ code/back-end/src/test/java/com/todolist/unit/controller/TaskListControllerTest.java
@@ -1,63 +1,63 @@
-// // package com.todolist.unit.controller;
+package com.todolist.unit.controller;
 
-// // import org.junit.jupiter.api.Test;
-// // import org.mockito.Mockito;
-// // import org.springframework.beans.factory.annotation.Autowired;
-// // import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-// // import org.springframework.boot.test.mock.mockito.MockBean;
-// // import org.springframework.http.MediaType;
-// // import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-// // import com.todolist.controller.TaskListController;
-// // import com.todolist.entity.TaskList;
-// // import com.todolist.service.TaskListService;
+import com.todolist.controller.TaskListController;
+import com.todolist.entity.TaskList;
+import com.todolist.service.TaskListService;
 
-// // import java.util.Arrays;
-// // import java.util.Collections;
+import java.util.Arrays;
+import java.util.Collections;
 
-// // import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-// // import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-// // @WebMvcTest(TaskListController.class)
-// // public class TaskListControllerTest {
+@WebMvcTest(TaskListController.class)
+public class TaskListControllerTest {
 
-// //     @Autowired
-// //     private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-// //     @MockBean
-// //     private TaskListService taskListService;
+    @MockBean
+    private TaskListService taskListService;
 
-// //     @Test
-// //     public void listAllTaskLists_ReturnsTaskLists() throws Exception {
-// //         TaskList taskList = new TaskList("lista 1", "Test Task List");
-// //         Mockito.when(taskListService.listAllTaskLists()).thenReturn(Arrays.asList(taskList));
+    @Test
+    public void listAllTaskLists_ReturnsTaskLists() throws Exception {
+        TaskList taskList = new TaskList("Lista 1", "Test Task List");
+        Mockito.when(taskListService.listAllTaskLists()).thenReturn(Arrays.asList(taskList));
 
-// //         mockMvc.perform(get("/api/taskLists"))
-// //                 .andExpect(status().isOk())
-// //                 .andExpect(jsonPath("$[0].id").value(taskList.getId()))
-// //                 .andExpect(jsonPath("$[0].name").value(taskList.getName()));
-// //     }
+        mockMvc.perform(get("/taskList/listAll"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(taskList.getId()))
+                .andExpect(jsonPath("$[0].name").value(taskList.getName()));
+    }
 
-// //     @Test
-// //     public void listAllTaskLists_ReturnsNoContent() throws Exception {
-// //         Mockito.when(taskListService.listAllTaskLists()).thenReturn(Collections.emptyList());
+    @Test
+    public void listAllTaskLists_ReturnsNoContent() throws Exception {
+        Mockito.when(taskListService.listAllTaskLists()).thenReturn(Collections.emptyList());
 
-// //         mockMvc.perform(get("/api/taskLists"))
-// //                 .andExpect(status().isNoContent());
-// //     }
+        mockMvc.perform(get("/taskList/listAll"))
+                .andExpect(status().isNoContent());
+    }
 
-// //     @Test
-// //     public void addTaskList_ReturnsCreatedTaskList() throws Exception {
-// //         TaskList taskList = new TaskList("lista 2", "Test Task List");
-// //         Mockito.when(taskListService.addTaskList(Mockito.any(TaskList.class))).thenReturn(taskList);
+    @Test
+    public void addTaskList_ReturnsCreatedTaskList() throws Exception {
+        TaskList taskList = new TaskList("Lista 2", "Test Task List");
+        Mockito.when(taskListService.addTaskList(Mockito.any(TaskList.class))).thenReturn(taskList);
 
-// //         mockMvc.perform(post("/api/taskLists")
-// //                 .contentType(MediaType.APPLICATION_JSON)
-// //                 .content("{\"name\": \"Test Task List\"}"))
-// //                 .andExpect(status().isCreated())
-// //                 .andExpect(jsonPath("$.id").value(taskList.getId()))
-// //                 .andExpect(jsonPath("$.name").value(taskList.getName()));
-// //     }
+        mockMvc.perform(post("/taskList/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\": \"Test Task List\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(taskList.getId()))
+                .andExpect(jsonPath("$.name").value(taskList.getName()));
+    }
 
-// //     // TODO More tests for update and delete
-// // }
+    // TODO More tests for update and delete
+}
